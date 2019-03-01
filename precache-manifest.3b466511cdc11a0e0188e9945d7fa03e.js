d1062b09857ae3e5b5553c4682ea5548c1e07891
Dellon Collins <dellon.collins@ad.infosys.com>
2019-02-28 19:34:07 -0500

    Updates

---
+++ precache-manifest.3b466511cdc11a0e0188e9945d7fa03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "691b86c76983bf19aebe",
+    "url": "/portfolio/static/js/main.691b86c7.chunk.js"
+  },
+  {
+    "revision": "572e6e5f5d68b6fcc5ab",
+    "url": "/portfolio/static/js/2.572e6e5f.chunk.js"
+  },
+  {
+    "revision": "691b86c76983bf19aebe",
+    "url": "/portfolio/static/css/main.c8b2427c.chunk.css"
+  },
+  {
+    "revision": "572e6e5f5d68b6fcc5ab",
+    "url": "/portfolio/static/css/2.bbf8805c.chunk.css"
+  },
+  {
+    "revision": "6ec689d8b7e80b8058ac21b9a1122fb8",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.4264e75a72a492d66c5c28f035a77fa8.js"
+  "/portfolio/precache-manifest.3b466511cdc11a0e0188e9945d7fa03e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/home/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
