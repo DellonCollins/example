4e613c50d5f8fe9a5750810c8f182325133eaa86
Dellon Collins <dellon.collins@ad.infosys.com>
2019-02-28 19:31:43 -0500

    updated navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,19 +3,15 @@ import React , {Component} from 'react'
 class NavBar extends Component{
     render(){
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
+            <nav class="navbar fixed-top navbar-light bg-light justify-content-center">
+                <div className="row">
+                    <a class="navbar-brand col mx-4">Home</a>
+                    <a class="navbar-brand col mx-4" href="#">Gallery</a>
+                    <a class="navbar-brand col mx-4" href="#">Contact</a>
                 </div>
-
             </nav>
         )
     }
 }
+
+export default NavBar;
