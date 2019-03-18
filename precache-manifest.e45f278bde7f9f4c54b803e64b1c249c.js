0b5bffd9368e76e97c0065d80de75d1edcbe5dc8
Dellon Collins <dellon.collins@ad.infosys.com>
2019-03-18 19:46:51 -0400

    Updates

---
+++ precache-manifest.e45f278bde7f9f4c54b803e64b1c249c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "33b9f99c8570eed950b9fe0358a589fa",
+    "url": "/portfolio/static/media/bg_polygon.33b9f99c.jpg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "815036a103d2e656402e",
+    "url": "/portfolio/static/js/main.815036a1.chunk.js"
+  },
+  {
+    "revision": "40d622ba4f5345ff257b",
+    "url": "/portfolio/static/js/2.40d622ba.chunk.js"
+  },
+  {
+    "revision": "815036a103d2e656402e",
+    "url": "/portfolio/static/css/main.ba48f9f0.chunk.css"
+  },
+  {
+    "revision": "40d622ba4f5345ff257b",
+    "url": "/portfolio/static/css/2.bbf8805c.chunk.css"
+  },
+  {
+    "revision": "8cb56d44e1a258c2d42fd6b3a16695b0",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0891f5ca955672d8e0abb548d3062b77.js"
+  "/portfolio/precache-manifest.e45f278bde7f9f4c54b803e64b1c249c.js"
 );
 
 workbox.clientsClaim();
