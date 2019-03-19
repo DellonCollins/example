59484fc848426f752d14c30042922ec8c850d26f
Dellon Collins <dellon.collins@ad.infosys.com>
2019-03-18 20:04:42 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.868e63ecbf5eb5ad5b723105acca4fa4.js"
+  "/portfolio/precache-manifest.ada0973483cd01ade501448eca097524.js"
 );
 
 workbox.clientsClaim();
